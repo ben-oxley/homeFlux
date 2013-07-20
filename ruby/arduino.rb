0ac647e15185262b3fed8108e4d872aa2310b10e
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-07-20 22:36:29 +0100

    brightness;

--- ruby/arduino.rb
+++ ruby/arduino.rb
@@ -5,9 +5,16 @@ require './light_control'
 
 
 ardunio = SerialCapture.new
+lights = LightControl.new('192.168.1.225')
+
 ardunio.start do |response|
   
   puts "chomp #{response.gsub("\n", "")}"
-  puts response.split(',')
+  unscaled_bri = response.split(',')[1]
+  
+  bri = (unscaled_bri / 1024) * 255
+  
+  lights.set_brightness(bri)
+  
 end
 
