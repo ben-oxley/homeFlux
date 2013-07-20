7cbfe31945eb6243d5a40076d6a3910827544f7a
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-07-20 21:00:40 +0100

    fiddling

--- ruby/arduino.rb
+++ ruby/arduino.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'serialport'
+require './lights/light_control'
 
 #We need to find out what port your arduino is on
 #and also what the corresponding file is on /dev
@@ -18,8 +19,12 @@ wait_time = 1/4.0 #in seconds
 
 #forever?
 loop do
+  
+  # value 1 = wheel position 0 - 255 represents 
+  # value 2 = 0 - 1024 light readying (200-900 ish)
+  # value 3 = desired intensity of light
   #arduino.write "whateveryouwanttowrite"
-  puts arduino.readline("\r") #read everything up to '\r'
+  puts current_line = arduino.readline("\r").chomp #read everything up to '\r'
 
   # wait a little bit before we read the next message?
   # I think we want to do something and restart reading immediately
