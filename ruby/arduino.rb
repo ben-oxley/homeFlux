2274b1822beefe37ca45ab225f842ea9ee0d77e2
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-07-20 23:57:12 +0100

    cycle thorugh the hours

--- ruby/arduino.rb
+++ ruby/arduino.rb
@@ -8,7 +8,7 @@ require './light_control'
 
 ardunio = SerialCapture.new
 lights = LightControl.new('192.168.1.225')
-@last_check = Time.new(0)
+count = 0
 
 ardunio.start do |response|
   
@@ -17,17 +17,13 @@ ardunio.start do |response|
   bri = (unscaled_bri.to_f / 1024.0) * 255.0
   lights.set_brightness(255 - bri.to_i)
   
-  # check temp
+ 
+  elevation = solar_elevation(DateTime.now - (count / 24), 51.5, -0.12) 
+  temperature = Temp.calculate_interpolated_temperature(elevation, Temp::DAY_TEMP, Temp::NIGHT_TEMP)
+  puts "(" + Time.now.to_s + "; " + elevation.to_s + "; " + temperature.to_s + ")"
   
-  if Time.now - 300 > @last_check 
-    puts 'once'
-    
-    elevation = solar_elevation(DateTime.now, 51.5, -0.12) 
-	  temperature = Temp.calculate_interpolated_temperature(elevation, Temp::DAY_TEMP, Temp::NIGHT_TEMP)
-	  puts "(" + Time.now.to_s + "; " + elevation.to_s + "; " + temperature.to_s + ")"
-	  
-	  @last_check = Time.now
-  end
   
+  count = 0 if count == 23
+
 end
 
