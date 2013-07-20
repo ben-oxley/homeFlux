a0023f6b096eb07de18cc66629944d95bc50b850
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-07-20 23:40:23 +0100

    fixes

--- ruby/arduino.rb
+++ ruby/arduino.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'serialport'
 require './solar'
+require './temp'
 require './serial_capture'
 require './light_control'
 
@@ -23,7 +24,7 @@ ardunio.start do |response|
     
     elevation = solar_elevation(DateTime.now, 51.5, -0.12) 
 	  temperature = Temp.calculate_interpolated_temperature(elevation, Temp::DAY_TEMP, Temp::NIGHT_TEMP)
-	  puts "(" + (date+i/24.0).to_s + "; " + elevation.to_s + "; " + temperature.to_s + ")"
+	  puts "(" + Time.now.to_s + "; " + elevation.to_s + "; " + temperature.to_s + ")"
 	  
 	  @last_check = Time.now
   end
