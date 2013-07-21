e52d25c435ea20bc26fce83dead308113a1a4615
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-07-21 01:47:28 +0100

    lasdasd

--- ruby/arduino.rb
+++ ruby/arduino.rb
@@ -23,10 +23,10 @@ ardunio.start do |response|
   temperature = Temp.calculate_interpolated_temperature(elevation, Temp::DAY_TEMP, Temp::NIGHT_TEMP)
   
   # on state
-  # on_state = !(bri == 0)
+  on_state = (bri.to_i != 0)
 
   # set the light data
-  lights.update_light_group({:on => true :bri => bri.to_i, :ct => temperature.to_i })
+  lights.update_light_group({:on => true, :bri => bri.to_i, :ct => temperature.to_i })
 
   count += 1
 
