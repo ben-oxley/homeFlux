24f12e0460038cd7337a0344c1963a93b0631489
mseri <marcello.seri@gmail.com>
2013-07-21 10:14:23 +0100

    arduino serial hard reset

---
+++ ruby/reset_arduino.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# usage: reset_arduino <DEVICE>
+# where <DEVICE> is typically some /dev/ttyfoobar
+import sys
+import serial
+ser = serial.Serial()
+ser.port='/dev/ttyACM0'  #sys.argv[1]
+ser.baudrate=1200
+ser.open(); ser.close();
