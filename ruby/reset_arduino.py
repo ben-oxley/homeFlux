12808f73b446927babc121aee8ea1eafdb4fb9de
mseri <marcello.seri@gmail.com>
2013-07-21 12:00:51 +0100

    lfdghoisgoj

--- ruby/reset_arduino.py
+++ ruby/reset_arduino.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import serial
+import time
 
 # cross fingers ?
 ser = serial.Serial()
 ser.port='/dev/ttyACM0'
 ser.baudrate=1200
-ser.open(); ser.close();
+ser.open(); time.sleep(2); ser.close();
